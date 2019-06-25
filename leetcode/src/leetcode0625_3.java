51b9ba877d2260c0fe4237575314f5d4624621a9
Admin <Admin@DESKTOP-8RBMPTQ>
2019-06-25 19:10:57 +0800

    三数之和

--- leetcode/src/leetcode0625_3.java
+++ leetcode/src/leetcode0625_3.java
@@ -4,43 +4,44 @@ import java.util.List;
 
 public class leetcode0625_3{
 	
-    public static List<List<Integer>> threeSum(int[] nums) {
-    	List<List<Integer>> result = new ArrayList<>();
-    	Arrays.sort(nums);
-    	for(int i=0;i<nums.length-2;i++)
-    	{
-    		int l=i+1,r=nums.length-1;
-    		if(nums[i]>0||nums[r]<0) break;
-    		while(l<r)
-    		{
-    			if(nums[l]<-nums[i]-nums[r])
-    			{
-    				l++;
-    				while(l<r&&nums[l]==nums[l-1])
-    				l++;
-    			}
-    		
-    			else if(nums[l]>-nums[i]-nums[r])
-    			{
-    				r--;
-    				while(l<r&&nums[r]==nums[r+1])
-        			r--;
-    			}
-    			else
-    			{
-    				result.add(Arrays.asList(nums[i],nums[l],nums[r]));
-    				l++;
-    				r--;
-    				while(l<r&&nums[l]==nums[l-1])
-    				l++;
-    				while(l<r&&nums[r]==nums[r+1])
-        			r--;
-    			}
-    		}
-    	}
-		return result;
+	public static List<List<Integer>> threeSum(int[] nums) {
+      	List<List<Integer>> result = new ArrayList<>();
+	Arrays.sort(nums);
+	for(int i=0;i<nums.length-2;i++)
+	{
+		int l=i+1,r=nums.length-1;
+		if(nums[i]>0||nums[r]<0) break;
+        if(i>0&&nums[i]==nums[i-1]) continue;
+		while(l<r)
+		{
+			if(nums[l]<-nums[i]-nums[r])
+			{
+				l++;
+				while(l<r&&nums[l]==nums[l-1])
+				l++;
+			}
+		
+			else if(nums[l]>-nums[i]-nums[r])
+			{
+				r--;
+				while(l<r&&nums[r]==nums[r+1])
+    			r--;
+			}
+			else
+			{
+				result.add(Arrays.asList(nums[i],nums[l],nums[r]));
+				l++;
+				r--;
+				while(l<r&&nums[l]==nums[l-1])
+				l++;
+				while(l<r&&nums[r]==nums[r+1])
+    			r--;
+			}
+		}
+	}
+	return result;  
+}
         
-    }
 	public static void main(String args[])
 	{
 		int[] nums= {-3,-6,-2,-4,4,7,1,2,3,2,-2};
