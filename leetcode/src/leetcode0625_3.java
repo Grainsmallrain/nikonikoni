d1f2ae3de8f72d18b3039b479dee0a6fe8a1ae51
Admin <Admin@DESKTOP-8RBMPTQ>
2019-06-25 19:15:27 +0800

    三数之和

--- leetcode/src/leetcode0625_3.java
+++ leetcode/src/leetcode0625_3.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class leetcode0625_3{
-	
+	  
 	public static List<List<Integer>> threeSum(int[] nums) {
-      	List<List<Integer>> result = new ArrayList<>();
+     List<List<Integer>> result = new ArrayList<>();
 	Arrays.sort(nums);
 	for(int i=0;i<nums.length-2;i++)
 	{
